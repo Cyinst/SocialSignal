405539a317457155e9878f408524e208fa3bd0d5
liuyu <russell44@126.com>
2024-03-20 18:48:36 +0800

    fix bug

--- app/libs/quote.ts
+++ app/libs/quote.ts
@@ -1,53 +1,53 @@
-import { ethers } from "ethers"
-import { computePoolAddress } from "@uniswap/v3-sdk"
-import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json"
-import { CurrentConfig } from "./config"
-import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json"
+// import { ethers } from "ethers"
+// import { computePoolAddress } from "@uniswap/v3-sdk"
+// import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json"
+// import { CurrentConfig } from "./config"
+// import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json"
 
-import { QUOTER_CONTRACT_ADDRESS } from "./constants"
-import { fromReadableAmount, toReadableAmount } from "./conversion"
+// import { QUOTER_CONTRACT_ADDRESS } from "./constants"
+// import { fromReadableAmount, toReadableAmount } from "./conversion"
 
-export async function quote(): Promise<string> {
-  const quoterContract = new ethers.Contract(
-    QUOTER_CONTRACT_ADDRESS,
-    Quoter.abi,
-    getProvider()
-  )
-  const poolConstants = await getPoolConstants()
+// export async function quote(): Promise<string> {
+//   const quoterContract = new ethers.Contract(
+//     QUOTER_CONTRACT_ADDRESS,
+//     Quoter.abi,
+//     getProvider()
+//   )
+//   const poolConstants = await getPoolConstants()
 
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    poolConstants.token0,
-    poolConstants.token1,
-    poolConstants.fee,
-    fromReadableAmount(
-      CurrentConfig.tokens.amountIn,
-      CurrentConfig.tokens.in.decimals
-    ).toString(),
-    0
-  )
+//   const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+//     poolConstants.token0,
+//     poolConstants.token1,
+//     poolConstants.fee,
+//     fromReadableAmount(
+//       CurrentConfig.tokens.amountIn,
+//       CurrentConfig.tokens.in.decimals
+//     ).toString(),
+//     0
+//   )
 
-  return toReadableAmount(quotedAmountOut, CurrentConfig.tokens.out.decimals)
-}
+//   return toReadableAmount(quotedAmountOut, CurrentConfig.tokens.out.decimals)
+// }
 
-const getPoolConstants = async () => {
-  const currentPoolAddress = computePoolAddress({
-    factoryAddress,
-    tokenA: CurrentConfig.tokens.in,
-    tokenB: CurrentConfig.tokens.out,
-    fee: CurrentConfig.tokens.poolFee,
-  })
+// const getPoolConstants = async () => {
+//   const currentPoolAddress = computePoolAddress({
+//     factoryAddress,
+//     tokenA: CurrentConfig.tokens.in,
+//     tokenB: CurrentConfig.tokens.out,
+//     fee: CurrentConfig.tokens.poolFee,
+//   })
 
-  const poolContract = new ethers.Contract(
-    currentPoolAddress,
-    IUniswapV3PoolABI.abi,
-    getProvider()
-  )
+//   const poolContract = new ethers.Contract(
+//     currentPoolAddress,
+//     IUniswapV3PoolABI.abi,
+//     getProvider()
+//   )
 
-  const [token0, token1, fee] = await Promise.all([
-    poolContract.token0(),
-    poolContract.token1(),
-    poolContract.fee(),
-  ])
+//   const [token0, token1, fee] = await Promise.all([
+//     poolContract.token0(),
+//     poolContract.token1(),
+//     poolContract.fee(),
+//   ])
 
-  return { token0, token1, fee }
-}
+//   return { token0, token1, fee }
+// }
